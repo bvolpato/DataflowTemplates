1d4e475052a9b0bcbc59a5c16b29ed4d9a0e2188
Bruno Volpato <bvolpato@google.com>
2023-01-27 10:08:42 -0500

    [Integration Tests] Create DirectRunner Client + Maven profile to run ITs the DirectRunner

--- it/src/main/java/com/google/cloud/teleport/it/kafka/DefaultKafkaResourceManager.java
+++ it/src/main/java/com/google/cloud/teleport/it/kafka/DefaultKafkaResourceManager.java
@@ -32,6 +32,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.Network;
 import org.testcontainers.utility.DockerImageName;
 
 /**
@@ -246,3 +247,5 @@ public class DefaultKafkaResourceManager extends TestContainerResourceManager<Ge
     }
   }
 }
+
+
