424ffe38ac504ae7fb230d6dfe8092a14f6fc7a2
Bruno Volpato <bvolpato@google.com>
2023-01-30 13:25:55 -0500

    [Plugin] Improve release target and parameter fixes

--- plugins/templates-maven-plugin/src/main/java/com/google/cloud/teleport/plugin/maven/TemplatesStageMojo.java
+++ plugins/templates-maven-plugin/src/main/java/com/google/cloud/teleport/plugin/maven/TemplatesStageMojo.java
@@ -370,11 +370,9 @@ public class TemplatesStageMojo extends TemplatesBaseMojo {
           "--metadata-file",
           outputClassesDirectory.getAbsolutePath() + "/" + metadataFile.getName(),
           "--additional-user-labels",
-          "{\"goog-dataflow-provided-template-name\":\""
-              + currentTemplateName
-              + "\", \"goog-dataflow-provided-template-version\":\""
-              + stagePrefix.toLowerCase()
-              + "\", \"goog-dataflow-provided-template-type\":\"flex\"}"
+          "goog-dataflow-provided-template-name="
+              + currentTemplateName.toLowerCase()
+              + ",goog-dataflow-provided-template-type=flex"
         };
     LOG.info("Running: {}", String.join(" ", flexTemplateBuildCmd));
 
