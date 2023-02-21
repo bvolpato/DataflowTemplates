ac76b24e4ea52adffe41af8e762dbc0f2f3fe609
Bruno Volpato <bvolpato@google.com>
2023-02-20 21:49:51 -0500

    [TextIOToBigQuery] Add RECORD support + integration tests

--- v2/googlecloud-to-googlecloud/src/test/java/com/google/cloud/teleport/v2/templates/TextIOToBigQueryTest.java
+++ v2/googlecloud-to-googlecloud/src/test/java/com/google/cloud/teleport/v2/templates/TextIOToBigQueryTest.java
@@ -101,11 +101,20 @@ public class TextIOToBigQueryTest {
             () -> TextIOToBigQuery.writeToBQTransform(options).withTestServices(bigQueryServices))
         .waitUntilFinish();
     assertThat(fakeDatasetService.getAllRows(PROJECT, DATASET, TABLE))
-        .isEqualTo(ImmutableList.of(new TableRow().set("BOOK_ID", "1").set("TITLE", "ABC")));
+        .isEqualTo(
+            ImmutableList.of(
+                new TableRow()
+                    .set("BOOK_ID", "1")
+                    .set("TITLE", "ABC")
+                    .set(
+                        "DETAILS",
+                        new TableRow()
+                            .set("YEAR", 2023)
+                            .set("SUMMARY", "LOREM IPSUM LOREM IPSUM"))));
   }
 
   @Test
-  public void testE2EWitStorageWriteApiAtLeastOnce() throws IOException, InterruptedException {
+  public void testE2EWithStorageWriteApiAtLeastOnce() throws IOException, InterruptedException {
     options.setUseStorageWriteApi(true);
     options.setUseStorageWriteApiAtLeastOnce(true);
 
@@ -114,11 +123,20 @@ public class TextIOToBigQueryTest {
             () -> TextIOToBigQuery.writeToBQTransform(options).withTestServices(bigQueryServices))
         .waitUntilFinish();
     assertThat(fakeDatasetService.getAllRows(PROJECT, DATASET, TABLE))
-        .isEqualTo(ImmutableList.of(new TableRow().set("book_id", "1").set("title", "ABC")));
+        .isEqualTo(
+            ImmutableList.of(
+                new TableRow()
+                    .set("book_id", "1")
+                    .set("title", "ABC")
+                    .set(
+                        "details",
+                        new TableRow()
+                            .set("year", "2023")
+                            .set("summary", "LOREM IPSUM LOREM IPSUM"))));
   }
 
   @Test
-  public void testE2EWitStorageWriteApi() throws IOException, InterruptedException {
+  public void testE2EWithStorageWriteApi() throws IOException, InterruptedException {
     options.setUseStorageWriteApi(true);
 
     TextIOToBigQuery.run(
@@ -126,6 +144,15 @@ public class TextIOToBigQueryTest {
             () -> TextIOToBigQuery.writeToBQTransform(options).withTestServices(bigQueryServices))
         .waitUntilFinish();
     assertThat(fakeDatasetService.getAllRows(PROJECT, DATASET, TABLE))
-        .isEqualTo(ImmutableList.of(new TableRow().set("book_id", "1").set("title", "ABC")));
+        .isEqualTo(
+            ImmutableList.of(
+                new TableRow()
+                    .set("book_id", "1")
+                    .set("title", "ABC")
+                    .set(
+                        "details",
+                        new TableRow()
+                            .set("year", "2023")
+                            .set("summary", "LOREM IPSUM LOREM IPSUM"))));
   }
 }
